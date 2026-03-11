017756f2a5ac1b66fde814a007580f951f72dc6e
Neptune <neptune@nepnep.xyz>
2026-03-11 00:26:56 -0700

    Added option to copy files instead of symlinking

---
+++ index.py
@@ -0,0 +1,22 @@
+from email.mime import audio
+
+from simple_term_menu import TerminalMenu
+import audioconfig.audioconfig as ac
+
+
+def main():
+    copy: bool = (ask_symlink_or_copy() == 0)
+    print("Final configs will be copied to destination" if copy else "Final configs will be symlinked to destination")
+    status = ac.main(copy=copy)
+    if status is Exception:
+        print(status)
+
+def ask_symlink_or_copy():
+    menu = TerminalMenu(['Copy Files', 'Symlink Files'], title='Copy or Symlink Files')
+    return menu.show()
+
+
+if __name__ == "__main__":
+    main()
+
+
--- setup-audio.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.config/pipewire/pipewire.conf.d
-mkdir -p ~/.config/wireplumber/wireplumber.conf.d
-
-echo linking files to pipewire.conf.d/
-ln -s ./audioconfig/generated/10-link-sinks.conf ~/.config/pipewire/pipewire.conf.d/
-
-echo linking files to wireplumber.d/
-ln -s ./audioconfig/wireplumber/10-audio-sinks.conf ~/.config/wireplumber/wireplumber.conf.d
-ln -s ./audioconfig/wireplumber/10-no-auto-gain.conf ~/.config/wireplumber/wireplumber.conf.d
